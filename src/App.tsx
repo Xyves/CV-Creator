1aceb433147b9dc5400cc213a43adba985b76c9e
Xyves <xodrixx@protonmail.com>
2024-03-03 21:58:31 +0100

    Update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-
+import PersonalData from "./components/Form/PersonalData";
+import Education from "./components/Form/Education";
+import Experience from "./components/Form/Experience";
+import Skills from "./components/Form/Skills";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="text-center selection:bg-green-900">
-      <h1>Works</h1>
+    <div className="sidebar max-w-sm bg-red-500">
+      <PersonalData></PersonalData>
+      <Education></Education>
+      <Experience></Experience>
+      <Skills></Skills>
     </div>
   );
 }
