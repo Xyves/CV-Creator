ca288b985570ee0f5552d4060415f47de1c449ba
Xyves <xodrixx@protonmail.com>
2024-03-08 21:37:04 +0100

    Add new data

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import PersonalData from "./components/Form/PersonalData";
 import Education from "./components/Form/Education";
 import Experience from "./components/Form/Experience";
 import Skills from "./components/Form/Skills";
-import Header from "./components/UI/Header";
+import Header from "./components/Form/Header";
 import MainResume from "./components/UI/MainResume";
 function App() {
   const [formData, setFormData] = useState({
@@ -24,13 +24,26 @@ function App() {
     schoolName: "University of Spain",
     startEdu: "10/19",
     endEdu: "2/23",
+    jobPosition2: "Front End Developer",
+    jobCompany2: "Netflix",
+    jobAddress2: "Warsaw",
+    isWorking2: true,
+    jobStartDate2: "01/2020",
+    jobEndDate2: "09/2022",
+    jobDescription2:
+      "As a Mid-Level Front-End Developer, I played a pivotal role in crafting engaging and user-centric web experiences for our customers.",
+    eduQuali2: "Bachelor of Science",
+    eduLocation2: "Madrid ",
+    schoolName2: "University of Spain",
+    startEdu2: "10/19",
+    endEdu2: "2/23",
   });
   const handleInputChange = (field, value) => {
     setFormData({ ...formData, [field]: value });
   };
   return (
-    <div className="container flex min-h-full min-w-full flex-wrap">
-      <div className="sidebar mx-10 max-w-xl bg-red-500">
+    <div className="container flex  min-h-full min-w-full flex-wrap ">
+      <div className="sidebar  max-w-xl overflow-y-scroll bg-red-500 px-10">
         <Header onInputChange={handleInputChange}></Header>
         <PersonalData onInputChange={handleInputChange}></PersonalData>
         <Education onInputChange={handleInputChange}></Education>
