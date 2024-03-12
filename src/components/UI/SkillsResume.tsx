8fa5c817f2a5b20371b3b45da276bc7b6249c50c
Xyves <xodrixx@protonmail.com>
2024-03-12 21:39:18 +0100

    Add tailwind styling

--- src/components/UI/SkillsResume.tsx
+++ src/components/UI/SkillsResume.tsx
@@ -1,14 +1,15 @@
 export default function SkillsResume({ data }) {
+  console.log(data);
   return (
     <ul>
       {Array.isArray(data.skill) ? (
         data.skill.map((item) => (
-          <li key={item.id} className="bg-green-400">
+          <li key={item.id} className="mb-5 block bg-green-400 p-3">
             {item.name}
           </li>
         ))
       ) : (
-        <li key={data.skill.id} className="bg-green-400">
+        <li key={data.skill.id} className="">
           {data.skill.name}
         </li>
       )}
